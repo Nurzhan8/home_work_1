57adafb5be36ec9c014567a6faf82925921a6fff
Nurzhan8 <bolotkyzy@gmail.com>
2023-11-02 21:34:46 +0600

    first commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,2 +1,16 @@
-git config --global user.name "Nurzhan8"
-git config --global user.email "bolotkyzy@gmail.com"
+import 'dart:io';
+
+void main() {
+  print('Введите порядковый номер пальца');
+  String finger = stdin.readLineSync()!;
+  if (finger == "1") {
+    print("большой палец");
+  } else if (finger == "2")
+    print("указательный палец");
+  else if (finger == "3")
+    print("средний палец");
+  else if (finger == "4")
+    print("Безымяный палец");
+  else if (finger == "5") print("мизинец");
+  else print ("error");
+}
